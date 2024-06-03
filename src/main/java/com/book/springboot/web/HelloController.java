38d26100784473c59ed7d6e2b9d12f177b086778
hyowonsong <woniwoni30@gmail.com>
2024-06-03 16:50:40 +0900

    [Add] HelloController 추가 및 단위테스트

---
+++ src/main/java/com/book/springboot/web/HelloController.java
@@ -0,0 +1,23 @@
+package com.book.springboot.web;
+
+import com.book.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// 컨트롤러를 JSON 반환하는 컨트롤러로 만들어줍니다.
+@RestController
+public class HelloController {
+
+    // HTTP Method 인 Get 의 요청을 받을 수 있는 API 를 만들어줍니다.
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}
